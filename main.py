406d701dd0b16e66b49659e217879383272c4c4c
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2023-09-20 13:30:18 -0300

    fix: modificação para que os botões ocupem toda a GUI principal

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from GraficoDeBarrasInsumos import GraficoDeBarrasRemedios, GraficoDeBarrasIAB, 
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
-from tkinter import messagebox  # Importe a biblioteca messagebox
+from tkinter import messagebox
 
 def mostrar_grafico(grafo_class):
     nova_janela = tk.Toplevel(window)
@@ -18,23 +18,39 @@ def mostrar_grafico(grafo_class):
 # Cria a janela principal
 window = tk.Tk()
 window.title('Exemplo de GUI com Gráfico')
-window.geometry('800x400')
 
-# Cria botões para cada tipo de gráfico
-botao_remedios = ttk.Button(window, text='Gráfico de Barras - Medicamentos', command=lambda: mostrar_grafico(GraficoDeBarrasRemedios))
-botao_iab = ttk.Button(window, text='Gráfico de Barras - Itens de Atenção Básica', command=lambda: mostrar_grafico(GraficoDeBarrasIAB))
-botao_vacinas_criancas = ttk.Button(window, text='Gráfico de Barras - Vacinas (Crianças)', command=lambda: mostrar_grafico(GraficoDeBarrasVacinasCriancas))
-botao_vacinas = ttk.Button(window, text='Gráfico de Barras - Vacinas (Adultos e Idosos)', command=lambda: mostrar_grafico(GraficoDeBarrasVacinas))
-botao_tr = ttk.Button(window, text='Gráfico de Barras - Testes Rápidos', command=lambda: mostrar_grafico(GraficoDeBarrasTR))
-botao_dg = ttk.Button(window, text='Gráfico de Barras - Itens de Distribuição Gratuita', command=lambda: mostrar_grafico(GraficoDeBarrasDG))
+# Preencha toda a largura e altura da GUI
+window.geometry('800x600')
+window.columnconfigure(0, weight=1)
+window.rowconfigure(1, weight=1)
 
-# Empacote os botões na janela
-botao_remedios.pack()
-botao_iab.pack()
-botao_vacinas_criancas.pack()
-botao_vacinas.pack()
-botao_tr.pack()
-botao_dg.pack()
+# Crie um painel para organizar os botões
+painel_de_botoes = ttk.LabelFrame(window, text='Opções', padding=10)
+painel_de_botoes.pack(fill='both', expand=True)
+
+# Crie um frame para preencher o espaço abaixo do painel de botões
+frame_vazio = ttk.Frame(window)
+frame_vazio.pack(fill='both', expand=True)
+
+# Crie um container para os botões
+container_de_botoes = ttk.Frame(painel_de_botoes)
+container_de_botoes.pack(fill='both', expand=True)
+
+# Crie botões para cada tipo de gráfico
+botao_remedios = ttk.Button(container_de_botoes, text='Gráfico de Barras - Medicamentos', command=lambda: mostrar_grafico(GraficoDeBarrasRemedios))
+botao_iab = ttk.Button(container_de_botoes, text='Gráfico de Barras - Itens de Atenção Básica', command=lambda: mostrar_grafico(GraficoDeBarrasIAB))
+botao_vacinas_criancas = ttk.Button(container_de_botoes, text='Gráfico de Barras - Vacinas (Crianças)', command=lambda: mostrar_grafico(GraficoDeBarrasVacinasCriancas))
+botao_vacinas = ttk.Button(container_de_botoes, text='Gráfico de Barras - Vacinas (Adultos e Idosos)', command=lambda: mostrar_grafico(GraficoDeBarrasVacinas))
+botao_tr = ttk.Button(container_de_botoes, text='Gráfico de Barras - Testes Rápidos', command=lambda: mostrar_grafico(GraficoDeBarrasTR))
+botao_dg = ttk.Button(container_de_botoes, text='Gráfico de Barras - Itens de Distribuição Gratuita', command=lambda: mostrar_grafico(GraficoDeBarrasDG))
+
+# Empacote os botões no container de botões
+botao_remedios.pack(fill='both', expand=True)
+botao_iab.pack(fill='both', expand=True)
+botao_vacinas_criancas.pack(fill='both', expand=True)
+botao_vacinas.pack(fill='both', expand=True)
+botao_tr.pack(fill='both', expand=True)
+botao_dg.pack(fill='both', expand=True)
 
 # Função para receber e salvar o arquivo CSV
 def receber_arquivo():
@@ -52,9 +68,9 @@ def receber_arquivo():
     except Exception as e:
         messagebox.showerror("Erro", f"Ocorreu um erro ao copiar o arquivo: {str(e)}")
 
-# Cria um botão para receber um arquivo CSV do usuário
-botao_arquivo = ttk.Button(window, text='Selecionar Arquivo CSV', command=receber_arquivo)
-botao_arquivo.pack()
+# Crie um botão para receber um arquivo CSV do usuário
+botao_arquivo = ttk.Button(painel_de_botoes, text='Selecionar Arquivo CSV', command=receber_arquivo)
+botao_arquivo.pack(fill='both', expand=True)
 
 # Inicia a interface gráfica
 window.mainloop()
