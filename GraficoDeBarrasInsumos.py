1a4082d758fc5b7b64d3b5684341aca6e699531b
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2023-09-20 12:29:04 -0300

    fixture: foi adicionado o atributo frame ao construtor de cada classe para que pudesse ser inicializado conforme era requerido na classe mãe

--- GraficoDeBarrasInsumos.py
+++ GraficoDeBarrasInsumos.py
@@ -1,38 +1,32 @@
 from GraficoDeBarrasBase import GraficoDeBarrasBase
 
-
 class GraficoDeBarrasRemedios(GraficoDeBarrasBase):
-    def __init__(self):
+    def __init__(self, frame):
         super().__init__("Entrada - Remedios", "Saida - Remedios",
-                         "Gráfico de Barras - Entrada e Saída de Medicamentos por Mês")
-
+                         "Gráfico de Barras - Entrada e Saída de Medicamentos por Mês", frame)
 
 class GraficoDeBarrasIAB(GraficoDeBarrasBase):
-    def __init__(self):
+    def __init__(self, frame):
         super().__init__("Entrada - IAB", "Saida - IAB",
-                         "Gráfico de Barras - Entrada e Saída de Itens de Atenção Básica por Mês")
+                         "Gráfico de Barras - Entrada e Saída de Itens de Atenção Básica por Mês", frame)
 
 class GraficoDeBarrasVacinasCriancas(GraficoDeBarrasBase):
-    def __init__(self):
+    def __init__(self, frame):
         super().__init__("Entrada - Vacinas1", "Saida - Vacinas1",
-                         "Gráfico de Barras - Entrada e Saída de Vacinas (Crianças) por Mês")
+                         "Gráfico de Barras - Entrada e Saída de Vacinas (Crianças) por Mês", frame)
 
 class GraficoDeBarrasVacinas(GraficoDeBarrasBase):
-    def __init__(self):
+    def __init__(self, frame):
         super().__init__("Entrada - Vacinas2", "Saida - Vacinas2",
-                         "Gráfico de Barras - Entrada e Saída de Vacinas (Adultos e Idosos) por Mês")
+                         "Gráfico de Barras - Entrada e Saída de Vacinas (Adultos e Idosos) por Mês", frame)
 
 class GraficoDeBarrasTR(GraficoDeBarrasBase):
-    def __init__(self):
+    def __init__(self, frame):
         super().__init__("Entrada - TR", "Saida - TR",
-                         "Gráfico de Barras - Entrada e Saída de Testes Rápidos por Mês")
+                         "Gráfico de Barras - Entrada e Saída de Testes Rápidos por Mês", frame)
 
 class GraficoDeBarrasDG(GraficoDeBarrasBase):
-    def __init__(self):
+    def __init__(self, frame):
         super().__init__("Entrada - DG", "Saida - DG",
-                         "Gráfico de Barras - Entrada e Saída de Itens de Distribuição Gratuita por Mês")
+                         "Gráfico de Barras - Entrada e Saída de Itens de Distribuição Gratuita por Mês", frame)
 
-
-# Exemplo de uso
-grafico1 = GraficoDeBarrasVacinasCriancas()
-grafico1.create_chart()
